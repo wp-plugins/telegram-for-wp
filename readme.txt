b64823e39a7e0e7c9cef297df82560fe7da85fdf
Amir Mousavi <Amir Mousavi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-18 13:35:58 +0000

    Adding first version of plugin
    git-svn-id: https://plugins.svn.wordpress.org/telegram-for-wp/trunk@1183498 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,34 @@
+=== Telegram for WP ===
+Contributors: amir-mousavi
+Tags: telegram,notifications,email
+Requires at least: 3.5
+Tested up to: 4.2.2
+Stable tag: trunk
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
+
+This plugin based on Notifygram service by Anton Ilzheev and allows WordPress site to send notifications to telegram.
+
+== Description ==
+Hi,
+
+If you are admin of a WordPress website and want to receive notifications about your website (comment submit, user register, new core update and etc.) in your telegram account, you are welcome to use this plugin.
+
+This plugin based on Notifygram service by Anton Ilzheev and allows WordPress site to send notifications to telegram.
+
+
+
+== Installation ==
+1. Download Telegram for WordPress plugin from github (https://github.com/Amir-Mousavi/notifygram-wordpress-plugin/archive/master.zip). 
+
+2. Upload zip file to your WordPress Plugins directory. /wp-content/plugins
+
+3. Activate the plugin through the \'Plugins\' menu in WordPress
+
+4. Connect Notifygram at https://notifygram.org
+
+5. Create a project
+
+6. Add telegram users you want to receive notifications
+
+7. Copy API key and API token of your project and paste them in plugin settings page
---
+++ twp.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * @package Telegram for Wordpress
+ * @version 1.0
+ */
+/*
+Plugin Name: Telegram for Wordpress
+Description: Allows user to recieve WordPress notifications in their Telegram account. This plugin based on notifygram.org by Anton Ilzheev.
+Author: Baloot Studio
+Version: 1.0
+Author URI: http://ameer.ir/
+License: GPLv2 or later.
+*/
+$twp_settings = 
+// create custom plugin settings menu
+add_action('admin_menu', 'twp_create_menu');
+function twp_create_menu() {
+	//create new top-level menu
+	add_menu_page('TWP Plugin Settings', 'TWP Settings', 'administrator', __FILE__, 'twp_settings_page',plugins_url('icon.png', __FILE__));
+	//call register settings function
+	add_action( 'admin_init', 'register_twp_settings' );
+}
+function register_twp_settings() {
+	//register our settings
+	register_setting( 'twp-settings-group', 'twp_api_key' );
+	register_setting( 'twp-settings-group', 'twp_api_token' );
+	register_setting( 'twp-settings-group', 'twp_project_name' );
+}
+function twp_settings_page() {
+?>
+<div class="wrap">
+    <h2>Telegram for WordPress</h2>
+    <form method="post" action="options.php">
+        <?php settings_fields( 'twp-settings-group' ); ?>
+        <table class="form-table">
+            <tr valign="top">
+                <th scope="row"><?php echo __("Instructions", "twp-plugin") ?></th>
+                <td><p><?php printf(__("Login into %s using phone number registered in Telegram. Notifygram will send confirmation code via Telegram. Once you receive code, enter it in Notifygram and click confirm. After login, in 'Add Project' section, enter a custom name; this could be anything but we recommend to use your site name. After adding project, you will see the project view (users, api , etc). You can also add other admin of your website in the list but they have to confirm using confirmation code. Click on API link at the right of the project name and copy paste those codes in this page. Save and Enjoy! ",  "twp-plugin"), "<a href='https://notifygram.org/login' target='_blank'>Notifygram</a>") ?> </p></td>
+            </tr>
+            <tr valign="top">
+                <th scope="row">API Key</th>
+                <td><input type="text" name="twp_api_key" value="<?php echo get_option('twp_api_key'); ?>" /></td>
+            </tr>
+            <tr valign="top">
+                <th scope="row">API Token</th>
+                <td><input type="text" name="twp_api_token" value="<?php echo get_option('twp_api_token'); ?>" /></td>
+            </tr>
+             <tr valign="top">
+                <th scope="row"><?php  echo __("Show Project name in messages", "twp-plugin") ?></th>
+                <td><input type="checkbox" name="twp_project_name" value="1" <?php checked( '1', get_option( 'twp_project_name' ) ); ?> /></td>
+            </tr>
+        </table>
+        <p class="submit">
+            <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+        </p>
+    </form>
+</div>
+<?php }
+    add_action( 'plugins_loaded', 'twp_load_textdomain' );
+    /**
+     * Load plugin textdomain.
+     *
+     * @since 1.0.0
+     */
+    function twp_load_textdomain() {
+      load_plugin_textdomain( 'twp-plugin', false, dirname( plugin_basename( __FILE__ ) ) . '/lang' ); 
+    }
+// Checks if TOKEN and API has been set. If not, show a warning message.
+if (get_option('twp_api_key') && get_option('twp_api_token') ) {
+    require_once("Notifygram.class.php");
+	//This will get information about sent mail from PHPMailer and send it to user
+    function twp_mail_action($result, $to, $cc, $bcc, $subject, $body){
+        $nt = new Notifygram();
+        $_apikey = get_option('twp_api_key');
+        $_apitoken = get_option('twp_api_token');
+        $_projectname = get_option('twp_project_name');
+        $nt->Notifygram($_apikey,$_apitoken, $_projectname );
+        $nt->notify($body);
+    }
+
+    /**
+     * Setup a custom PHPMailer action callback. This will let us to fire our action every time a mail sent
+     * Thanks to Birgire (http://xlino.com/) for creating this code snippet.
+     */
+    add_action( 'phpmailer_init', function( $phpmailer ){
+        $phpmailer->action_function = 'twp_mail_action';
+        } );
+    
+    } else {
+        function twp_api_error_notice() {
+        $class = "error";
+        $message = sprintf(__('Your API key or API token are not set. Please go to %s and set them.','twp-plugin'), "<a href='".admin_url('admin.php?page=twp/twp.php')."'>".__("TWP Settings", "twp-plugin")."</a>");
+            echo"<div class=\"$class\"> <p>$message</p></div>"; 
+        }
+    add_action( 'admin_notices', 'twp_api_error_notice' ); 
+    }
